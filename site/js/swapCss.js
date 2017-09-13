536457eed5f74af77ce9f1b9165f59a5dee69b42
ALREstevam <a166348@g.unicamp.com>
2017-09-13 16:54:57 -0300

    atz2

--- site/js/swapCss.js
+++ site/js/swapCss.js
@@ -10,11 +10,33 @@ function calcNextElem(total, current) {
 }
 
 
+function restoreLastSheet(linkid) {
+    console.log('RESTORING');
+
+    if (typeof(Storage) !== "undefined") {
+        console.log('IS UNDEFINED');
+        if(localStorage.length == 0){
+            var style = nextSheet(linkid);
+            console.log('SETTING TO '+style);
+            localStorage.setItem("cssswap", style);
+        }else{
+            document.getElementById(linkid).href = localStorage.getItem('cssswap');
+        }
+
+    } else {
+        console.log('No Web Storage support')
+    }
+}
+
 function nextSheet(linkid) {
+    console.log('NEXSHEET');
+
     if(sheetList.length > 0){
         currentSheet = calcNextElem(sheetList.length, currentSheet);
         document.getElementById(linkid).href = sheetList[currentSheet];
+        return sheetList[currentSheet];
     }else{
         console.log('No stylesheet was set to make the swap.')
+        return null;
     }
 }
