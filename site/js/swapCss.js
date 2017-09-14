24e615e568166dfaa02ce118aaaf4c3395d01b49
ALREstevam <a166348@g.unicamp.com>
2017-09-13 22:09:24 -0300

    at3

--- site/js/swapCss.js
+++ site/js/swapCss.js
@@ -1,5 +1,5 @@
 var sheetList = [];
-var currentSheet = 0;
+var currentSheet = 2;
 
 function setSheetList(infoArr) {
     sheetList = infoArr;
@@ -9,39 +9,18 @@ function calcNextElem(total, current) {
     return (current + 1) % total;
 }
 
-
-function restoreLastSheet(linkid) {
-    console.log('RESTORING');
-
-    if (typeof(Storage) !== "undefined") {
-        console.log('IS UNDEFINED');
-        if(localStorage.getItem("cssswap") == null){
-            var style = nextSheet(linkid);
-            console.log('SETTING TO: '+style);
-            localStorage.setItem("cssswap", style);
-        }else{
-            console.log('FOUND LAST ' + localStorage.getItem('cssswap'));
-            document.getElementById(linkid).href = sheetList[localStorage.getItem('cssswap')];
-        }
-
-    } else {
-        console.log('No Web Storage support')
-    }
+function nextSheet(linkId){
+    currentSheet = calcNextElem(sheetList.length, currentSheet);
+    document.getElementById(linkId).href = sheetList[currentSheet];
+    localStorage.setItem('cssswap',currentSheet);
 }
 
-function nextSheet(linkid) {
-    console.log('NEXSHEET');
+function restoreLastSheet(linkid) {
+    if (!localStorage.getItem('cssswap')) {
+        nextSheet(linkid);
 
-    if(sheetList.length > 0){
-
-        currentSheet = calcNextElem(sheetList.length, currentSheet);
-        
-        console.log("SETTING: "+currentSheet)
+    } else {
+        currentSheet = localStorage.getItem('cssswap');
         document.getElementById(linkid).href = sheetList[currentSheet];
-        localStorage.setItem('cssswap', currentSheet);
-        return currentSheet;
-    }else{
-        console.log('No stylesheet was set to make the swap.')
-        return -1;
     }
 }
